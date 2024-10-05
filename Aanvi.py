92979c0f913b7e21adaeec430931dfbd8f130416
Aanviag <aanviag15@gmail.com>
2024-10-04 20:57:55 -0400

    Add files via upload

---
+++ Aanvi.py
@@ -0,0 +1,43 @@
+
+#variables in first sentence
+name = input("Enter a name")
+place = input("Enter a place")
+word = input("Enter a description")
+
+#line 1
+print("On a sunny day, " + name + " was walking down a " + word +  place)
+
+#variables used in next part
+animal = input("Enter a animal")
+trait = input("Enter a characteristic an animal doesn't have")
+
+#story part 2
+print("When suddenly a " + animal +" jumped out in front of her." )
+print("The " + animal + " could " + trait )
+print("The "+ trait + "ing " + animal + " came near the " + name)
+
+#variable for 3rd part of story
+animal_name = input("Enter a animal name")
+color = input("Enter a color")
+object = input("Enter a object")
+house = input("Enter a candy")
+verb = input("Enter a excited action")
+number = input("Enter a number")
+
+#story continued
+print("The " + animal + " was named " + animal_name)
+print(animal_name + " handed a " + color + object )
+print("The " + object + " read " + " FOLLOW THE " + animal + " THEN YOU WILL FIND A HOUSE. " )
+print("To "+ name + "'s " + " surprise, there it was. Filled with" + number + candy + "ies.")
+
+#Full story
+print("On a sunny day, " + name + " was walking down a " + adj + place )
+print("When suddenly a " + animal +" jumped out in front of her." )
+print("The " + animal + " could " + trait )
+print("The "+ trait + "ing " + animal + " came near the " + name)
+print("The " + animal + " was named " + animal_name)
+print(animal_name + " handed a " + color + object )
+print("The " + object + " read " + " FOLLOW THE " + animal + " THEN YOU WILL FIND A HOUSE. " )
+print("To "+ name + "'s " + " surprise, there it was. Filled with" + number + candy +"ies. She was very" + verb)
+print(name + "screamed with excitment!" )
+
